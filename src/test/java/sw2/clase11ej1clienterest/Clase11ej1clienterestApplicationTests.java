aaa53511b138be9b0bc93eb475d8edc1cda04352
Stuardo <stuardo.lucho@pucp.pe>
2020-02-27 04:56:28 -0500

    proyecto base

---
+++ src/test/java/sw2/clase11ej1clienterest/Clase11ej1clienterestApplicationTests.java
@@ -0,0 +1,13 @@
+package sw2.clase11ej1clienterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase11ej1clienterestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
